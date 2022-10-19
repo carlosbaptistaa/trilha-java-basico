6c14f6583856e76207116a1c5c150fb0f7bbbc80
Carlos Eduardo <carlos.baptistaa@hotmail.com>
2022-10-19 20:29:42 -0300

    atualizacao

---
+++ tipos-variaveis/src/OperadoresIncremento.java
@@ -0,0 +1,12 @@
+public class OperadoresIncremento {
+
+    public static void main(String[]args) {
+
+        int numero = 5; /*gostaria que o numero incremente de 2 em 2 */
+
+            numero = numero + 2;
+
+        System.out.println(numero);
+    }
+    
+}
---
+++ tipos-variaveis/src/OperadoresUnarios.java
@@ -0,0 +1,15 @@
+public class OperadoresUnarios {
+
+    public static void main(String[] args) {
+
+        int numero = 5;
+
+        numero = - numero; /*o numero positivo converterá em negativo */
+
+        System.out.println(numero);
+
+        numero = numero * -1; /*para transformar o numero negativo em positivo, deve multipl. por -1 */
+        System.out.println(numero);
+        
+    }
+}
