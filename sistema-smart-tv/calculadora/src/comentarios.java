d97293761e0a1be264c2a568078c3ede99ee2b01
Carlos Baptista <103068255+carlosbaptistaa@users.noreply.github.com>
2022-10-20 12:15:41 -0300

    atualizacao.

---
+++ sistema-smart-tv/calculadora/src/comentarios.java
@@ -0,0 +1,16 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        //Comentário em uma única linha
+        /*Comentario
+         * em bloco
+         */
+
+    } 
+    public void metodo() {
+        /**comentario
+         * usado 
+         * para documentacao
+         * 
+         */
+    }
+}
